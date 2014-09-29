3aa2f72f7e592c6df71f5b641ad2eb17026bc9da
Arnaud Marant <arnaudmarant@gmail.com>
2014-09-29 13:39:21 +0200

    Add bindings for image methods, create ImageStatic facade like PechkinStatic.

---
+++ Pechkin/ImageStatic.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using TuesPechkin.Util;
+
+namespace TuesPechkin
+{
+    /// <summary>
+    /// Static class with utility methods for the interface.
+    /// Acts mostly as a facade over PechkinBindings with log tracing.
+    /// </summary>
+    [Serializable]
+    internal static class ImageStatic
+    {
+        public static IntPtr CreateGlobalSetting()
+        {
+            Tracer.Trace("T:" + Thread.CurrentThread.Name + " Creating global settings (wkhtmltoimage_create_global_settings)");
+
+            return PechkinBindings.wkhtmltoimage_create_global_settings();
+        }
+
+        public static int SetGlobalSetting(IntPtr setting, string name, string value)
+        {
+            Tracer.Trace(
+                String.Format(
+                    "T:{0} Setting global setting '{1}' to '{2}' for config {3}",
+                    Thread.CurrentThread.Name,
+                    name,
+                    value,
+                    setting));
+
+            return PechkinBindings.wkhtmltoimage_set_global_setting(setting, name, value);
+        }
+
+        public static string GetGlobalSetting(IntPtr setting, string name)
+        {
+            Tracer.Trace("T:" + Thread.CurrentThread.Name + " Getting global setting (wkhtmltoimage_get_global_setting)");
+
+            byte[] buf = new byte[2048];
+
+            PechkinBindings.wkhtmltoimage_get_global_setting(setting, name, ref buf, buf.Length);
+
+            int walk = 0;
+            while (walk < buf.Length && buf[walk] != 0)
+            {
+                walk++;
+            }
+
+            byte[] buf2 = new byte[walk];
+            Array.Copy(buf, 0, buf2, 0, walk);
+
+            return Encoding.UTF8.GetString(buf2);
+        }
+
+        public static IntPtr CreateConverter(IntPtr globalSettings, byte[] data)
+        {
+            Tracer.Trace("T:" + Thread.CurrentThread.Name + " Creating converter (wkhtmltoimage_create_converter)");
+
+            return PechkinBindings.wkhtmltoimage_create_converter(globalSettings, data);
+        }
+
+        public static void DestroyConverter(IntPtr converter)
+        {
+            Tracer.Trace("T:" + Thread.CurrentThread.Name + " Destroying converter (wkhtmltoimage_destroy_converter)");
+
+            PechkinBindings.wkhtmltoimage_destroy_converter(converter);
+        }
+
+        public static void SetWarningCallback(IntPtr converter, StringCallback callback)
+        {
+            Tracer.Trace("T:" + Thread.CurrentThread.Name + " Setting warning callback (wkhtmltoimage_set_warning_callback)");
+            
+            PechkinBindings.wkhtmltoimage_set_warning_callback(converter, callback);
+        }
+
+        public static void SetErrorCallback(IntPtr converter, StringCallback callback)
+        {
+            Tracer.Trace("T:" + Thread.CurrentThread.Name + " Setting error callback (wkhtmltoimage_set_error_callback)");
+            
+            PechkinBindings.wkhtmltoimage_set_error_callback(converter, callback);
+        }
+
+        public static void SetFinishedCallback(IntPtr converter, IntCallback callback)
+        {
+            Tracer.Trace("T:" + Thread.CurrentThread.Name + " Setting finished callback (wkhtmltoimage_set_finished_callback)");
+
+            PechkinBindings.wkhtmltoimage_set_finished_callback(converter, callback);
+        }
+
+        public static void SetPhaseChangedCallback(IntPtr converter, VoidCallback callback)
+        {
+            Tracer.Trace("T:" + Thread.CurrentThread.Name + " Setting phase change callback (wkhtmltoimage_set_phase_changed_callback)");
+
+            PechkinBindings.wkhtmltoimage_set_phase_changed_callback(converter, callback);
+        }
+
+        public static void SetProgressChangedCallback(IntPtr converter, IntCallback callback)
+        {
+            Tracer.Trace("T:" + Thread.CurrentThread.Name + " Setting progress change callback (wkhtmltoimage_set_progress_changed_callback)");
+
+            PechkinBindings.wkhtmltoimage_set_progress_changed_callback(converter, callback);
+        }
+
+        public static bool PerformConversion(IntPtr converter)
+        {
+            Tracer.Trace("T:" + Thread.CurrentThread.Name + " Starting conversion (wkhtmltoimage_convert)");
+
+            return PechkinBindings.wkhtmltoimage_convert(converter) != 0;
+        }
+
+        public static int GetPhaseNumber(IntPtr converter)
+        {
+            Tracer.Trace("T:" + Thread.CurrentThread.Name + " Requesting current phase (wkhtmltoimage_current_phase)");
+
+            return PechkinBindings.wkhtmltoimage_current_phase(converter);
+        }
+
+        public static int GetPhaseCount(IntPtr converter)
+        {
+            Tracer.Trace("T:" + Thread.CurrentThread.Name + " Requesting phase count (wkhtmltoimage_phase_count)");
+
+            return PechkinBindings.wkhtmltoimage_phase_count(converter);
+        }
+
+        public static string GetPhaseDescription(IntPtr converter, int phase)
+        {
+            Tracer.Trace("T:" + Thread.CurrentThread.Name + " Requesting phase description (wkhtmltoimage_phase_description)");
+
+            return Marshal.PtrToStringAnsi(PechkinBindings.wkhtmltoimage_phase_description(converter, phase));
+        }
+
+        public static string GetProgressDescription(IntPtr converter)
+        {
+            Tracer.Trace("T:" + Thread.CurrentThread.Name + " Requesting progress string (wkhtmltoimage_progress_string)");
+
+            return Marshal.PtrToStringAnsi(PechkinBindings.wkhtmltoimage_progress_string(converter));
+        }
+
+        public static int GetHttpErrorCode(IntPtr converter)
+        {
+            Tracer.Trace("T:" + Thread.CurrentThread.Name + " Requesting http error code (wkhtmltoimage_http_error_code)");
+
+            return PechkinBindings.wkhtmltoimage_http_error_code(converter);
+        }
+
+        public static byte[] GetConverterResult(IntPtr converter)
+        {
+            Tracer.Trace("T:" + Thread.CurrentThread.Name + " Requesting converter result (wkhtmltoimage_get_output)");
+
+            IntPtr tmp;
+            var len = PechkinBindings.wkhtmltoimage_get_output(converter, out tmp);
+            var output = new byte[len];
+            Marshal.Copy(tmp, output, 0, output.Length);
+            return output;
+        }
+    }
+}
--- Pechkin/PechkinBindings.cs
+++ Pechkin/PechkinBindings.cs
@@ -20,38 +20,115 @@ namespace TuesPechkin
             SetupUnmanagedAssembly("wkhtmltox.dll", raw);
         }
 
+        /// <summary>
+        /// CAPI(void) wkhtmltopdf_add_object(wkhtmltopdf_converter * converter, wkhtmltopdf_object_settings * setting, const char * data);
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern void wkhtmltopdf_add_object(IntPtr converter, IntPtr objectSettings,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8Marshaler))]
             String data);
 
+        /// <summary>
+        /// CAPI(void) wkhtmltopdf_add_object(wkhtmltopdf_converter * converter, wkhtmltopdf_object_settings * setting, const char * data);
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern void wkhtmltopdf_add_object(IntPtr converter, IntPtr objectSettings, byte[] data);
 
+        /// <summary>
+        /// CAPI(int) wkhtmltopdf_convert(wkhtmltopdf_converter * converter)
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern int wkhtmltopdf_convert(IntPtr converter);
 
+        /// <summary>
+        /// CAPI(int) wkhtmltoimage_convert(wkhtmltoimage_converter * converter)
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern int wkhtmltoimage_convert(IntPtr converter);
+
+        /// <summary>
+        /// CAPI(wkhtmltopdf_converter *) wkhtmltopdf_create_converter(wkhtmltopdf_global_settings * settings)
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern IntPtr wkhtmltopdf_create_converter(IntPtr globalSettings);
 
+        /// <summary>
+        /// CAPI(wkhtmltoimage_converter *) wkhtmltoimage_create_converter(wkhtmltoimage_global_settings * settings, const char * data)
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr wkhtmltoimage_create_converter(IntPtr globalSettings, byte[] data);
+
+        /// <summary>
+        /// CAPI(wkhtmltopdf_global_settings *) wkhtmltopdf_create_global_settings()
+        /// </summary>
+        /// <returns></returns>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern IntPtr wkhtmltopdf_create_global_settings();
 
+        /// <summary>
+        /// CAPI(wkhtmltoimage_global_settings *) wkhtmltoimage_create_global_settings()
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr wkhtmltoimage_create_global_settings();
+
+        /// <summary>
+        /// CAPI(wkhtmltopdf_object_settings *) wkhtmltopdf_create_object_settings()
+        /// </summary>
+        /// <returns></returns>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern IntPtr wkhtmltopdf_create_object_settings();
 
+        /// <summary>
+        /// CAPI(int) wkhtmltopdf_current_phase(wkhtmltopdf_converter * converter)
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern int wkhtmltopdf_current_phase(IntPtr converter);
 
+        /// <summary>
+        /// CAPI(int) wkhtmltoimage_current_phase(wkhtmltoimage_converter * converter)
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern int wkhtmltoimage_current_phase(IntPtr converter);
+
+        /// <summary>
+        /// CAPI(int) wkhtmltopdf_deinit()
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern int wkhtmltopdf_deinit();
 
+        /// <summary>
+        /// CAPI(int) wkhtmltoimage_deinit()
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern int wkhtmltoimage_deinit();
+
+        /// <summary>
+        /// CAPI(void) wkhtmltopdf_destroy_converter(wkhtmltopdf_converter * converter)
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern void wkhtmltopdf_destroy_converter(IntPtr converter);
 
+        /// <summary>
+        /// CAPI(void) wkhtmltoimage_destroy_converter(wkhtmltoimage_converter * converter)
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern void wkhtmltoimage_destroy_converter(IntPtr converter);
+
+        /// <summary>
+        /// CAPI(int) wkhtmltopdf_extended_qt()
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern int wkhtmltopdf_extended_qt();
 
+        /// <summary>
+        /// CAPI(int) wkhtmltoimage_extended_qt()
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern int wkhtmltoimage_extended_qt();
+
+        /// <summary>
+        /// CAPI(int) wkhtmltopdf_get_global_setting(wkhtmltopdf_global_settings * settings, const char * name, char * value, int vs)
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern int wkhtmltopdf_get_global_setting(IntPtr settings,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8Marshaler))]
@@ -60,6 +137,20 @@ namespace TuesPechkin
             [Out]
             ref byte[] value, int valueSize);
 
+        /// <summary>
+        /// CAPI(int) wkhtmltoimage_get_global_setting(wkhtmltoimage_global_settings * settings, const char * name, char * value, int vs)
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern int wkhtmltoimage_get_global_setting(IntPtr settings,
+            [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8Marshaler))]
+            String name,
+            [In]
+            [Out]
+            ref byte[] value, int valueSize);
+
+        /// <summary>
+        /// CAPI(int) wkhtmltopdf_get_object_setting(wkhtmltopdf_object_settings * settings, const char * name, char * value, int vs)
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern int wkhtmltopdf_get_object_setting(IntPtr settings,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8Marshaler))]
@@ -68,32 +159,110 @@ namespace TuesPechkin
             [Out]
             ref byte[] value, int vs);
 
+
+        /// <summary>
+        /// CAPI(long) wkhtmltopdf_get_output(wkhtmltopdf_converter * converter, const unsigned char **)
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern int wkhtmltopdf_get_output(IntPtr converter, out IntPtr data);
 
+        /// <summary>
+        /// CAPI(long) wkhtmltoimage_get_output(wkhtmltoimage_converter * converter, const unsigned char **)
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern int wkhtmltoimage_get_output(IntPtr converter, out IntPtr data);
+
+        /// <summary>
+        /// CAPI(int) wkhtmltopdf_http_error_code(wkhtmltopdf_converter * converter)
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern int wkhtmltopdf_http_error_code(IntPtr converter);
 
+        /// <summary>
+        /// CAPI(int) wkhtmltoimage_http_error_code(wkhtmltoimage_converter * converter)
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern int wkhtmltoimage_http_error_code(IntPtr converter);
+
+        /// <summary>
+        /// CAPI(int) wkhtmltopdf_init(int use_graphics);
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern int wkhtmltopdf_init(int useGraphics);
 
+        /// <summary>
+        /// CAPI(int) wkhtmltoimage_init(int use_graphics)
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern int wkhtmltoimage_init(int useGraphics);
+
+        /// <summary>
+        /// CAPI(int) wkhtmltopdf_phase_count(wkhtmltopdf_converter * converter)
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern int wkhtmltopdf_phase_count(IntPtr converter);
 
+        /// <summary>
+        /// CAPI(int) wkhtmltoimage_phase_count(wkhtmltoimage_converter * converter)
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern int wkhtmltoimage_phase_count(IntPtr converter);
+
+        /// <summary>
+        /// CAPI(const char *) wkhtmltopdf_phase_description(wkhtmltopdf_converter * converter, int phase)
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern IntPtr wkhtmltopdf_phase_description(IntPtr converter, int phase);
 
+        /// <summary>
+        /// CAPI(const char *) wkhtmltoimage_phase_description(wkhtmltoimage_converter * converter, int phase)
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr wkhtmltoimage_phase_description(IntPtr converter, int phase);
+
+        /// <summary>
+        /// CAPI(const char *) wkhtmltopdf_progress_string(wkhtmltopdf_converter * converter)
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern IntPtr wkhtmltopdf_progress_string(IntPtr converter);
 
+        /// <summary>
+        /// CAPI(const char *) wkhtmltoimage_progress_string(wkhtmltoimage_converter * converter)
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr wkhtmltoimage_progress_string(IntPtr converter);
+
+        /// <summary>
+        /// CAPI(void) wkhtmltopdf_set_error_callback(wkhtmltopdf_converter * converter, wkhtmltopdf_str_callback cb)
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern void wkhtmltopdf_set_error_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)]
                                                                  StringCallback callback);
 
+        /// <summary>
+        /// CAPI(void) wkhtmltoimage_set_error_callback(wkhtmltoimage_converter * converter, wkhtmltoimage_str_callback cb)
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern void wkhtmltoimage_set_error_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)]
+                                                                 StringCallback callback);
+
+        /// <summary>
+        /// CAPI(void) wkhtmltopdf_set_finished_callback(wkhtmltopdf_converter * converter, wkhtmltopdf_int_callback cb)
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern void wkhtmltopdf_set_finished_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)]
                                                                     IntCallback callback);
 
+        /// <summary>
+        /// CAPI(void) wkhtmltoimage_set_finished_callback(wkhtmltoimage_converter * converter, wkhtmltoimage_int_callback cb);
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern void wkhtmltoimage_set_finished_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)]
+                                                                    IntCallback callback);
+
+        /// <summary>
+        /// CAPI(int) wkhtmltopdf_set_global_setting(wkhtmltopdf_global_settings * settings, const char * name, const char * value)
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern int wkhtmltopdf_set_global_setting(IntPtr settings,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8Marshaler))]
@@ -101,6 +270,19 @@ namespace TuesPechkin
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8Marshaler))]
             String value);
 
+        /// <summary>
+        /// CAPI(int) wkhtmltoimage_set_global_setting(wkhtmltoimage_global_settings * settings, const char * name, const char * value)
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern int wkhtmltoimage_set_global_setting(IntPtr settings,
+            [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8Marshaler))]
+            String name,
+            [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8Marshaler))]
+            String value);
+
+        /// <summary>
+        /// CAPI(int) wkhtmltopdf_set_object_setting(wkhtmltopdf_object_settings * settings, const char * name, const char * value)
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern int wkhtmltopdf_set_object_setting(IntPtr settings,
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8Marshaler))]
@@ -108,21 +290,60 @@ namespace TuesPechkin
             [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(Utf8Marshaler))]
             String value);
 
+        /// <summary>
+        /// CAPI(void) wkhtmltopdf_set_phase_changed_callback(wkhtmltopdf_converter * converter, wkhtmltopdf_void_callback cb)
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern void wkhtmltopdf_set_phase_changed_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)]
                                                                          VoidCallback callback);
+        /// <summary>
+        /// CAPI(void) wkhtmltoimage_set_phase_changed_callback(wkhtmltoimage_converter * converter, wkhtmltoimage_void_callback cb)
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern void wkhtmltoimage_set_phase_changed_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)]
+                                                                         VoidCallback callback);
 
+        /// <summary>
+        /// CAPI(void) wkhtmltopdf_set_progress_changed_callback(wkhtmltopdf_converter * converter, wkhtmltopdf_int_callback cb)
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern void wkhtmltopdf_set_progress_changed_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)]
                                                                             IntCallback callback);
 
+        /// <summary>
+        /// CAPI(void) wkhtmltoimage_set_progress_changed_callback(wkhtmltoimage_converter * converter, wkhtmltoimage_int_callback cb)
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern void wkhtmltoimage_set_progress_changed_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)]
+                                                                            IntCallback callback);
+
+        /// <summary>
+        /// CAPI(void) wkhtmltopdf_set_warning_callback(wkhtmltopdf_converter * converter, wkhtmltopdf_str_callback cb)
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         public static extern void wkhtmltopdf_set_warning_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)]
                                                                    StringCallback callback);
 
+        /// <summary>
+        /// CAPI(void) wkhtmltoimage_set_warning_callback(wkhtmltoimage_converter * converter, wkhtmltoimage_str_callback cb)
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        public static extern void wkhtmltoimage_set_warning_callback(IntPtr converter, [MarshalAs(UnmanagedType.FunctionPtr)]
+                                                                   StringCallback callback);
+
+        /// <summary>
+        /// CAPI(const char *) wkhtmltopdf_version();
+        /// </summary>
         [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
         [return: MarshalAs(UnmanagedType.LPStr)]
         public static extern String wkhtmltopdf_version();
+
+        /// <summary>
+        /// CAPI(const char *)wkhtmltoimage_version()
+        /// </summary>
+        [DllImport("wkhtmltox.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall)]
+        [return: MarshalAs(UnmanagedType.LPStr)]
+        public static extern String wkhtmltoimage_version();
 
         private static void SetupUnmanagedAssembly(string fileName, byte[] assemblyRaw)
         {
